601ec84ba3e0ca884a53319800bdab6d4c771e02
Tama Dioo <tamadio91@gmail.com>
2023-10-09 11:21:19 +0700

    Commit dan push percobaan 1 jobsheet 7

---
+++ ForKelipatan09.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ForKelipatan09 {
+    public static void main(String[] args) {
+        //Deklarasi scanner
+        Scanner scan = new Scanner(System.in);
+
+        //Inisialisasi variabel
+        int kelipatan, jumlah = 0, counter = 0;
+
+        //Kode untuk meminta input dari keyboard
+        System.out.print("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
+
+        //Struktur perulangan for dengan kondisi pemilihan if untuk menentukan bilangan kelipatan.
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+        //Tampilkan banyaknya bilangan kelipatan dan total bilangan kelipatan pada rentang 1 sampai dengan 50.
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+    }
+}
