48e50a8d77aded260ee4d974383e97d5583dcd01
Tama Dioo <tamadio91@gmail.com>
2023-10-09 16:52:29 +0700

    Menambahkan Latihan do while

---
+++ Dowhile09.java
@@ -0,0 +1,9 @@
+public class Dowhile09 {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+    }
+}
