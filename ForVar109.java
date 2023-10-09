82fc8a7231b97113cb204e431a9169029c360372
Tama Dioo <tamadio91@gmail.com>
2023-10-09 12:40:39 +0700

    Menambahkan Latihan for variasi 1

---
+++ ForVar109.java
@@ -0,0 +1,7 @@
+public class ForVar109 {
+    public static void main(String[] args) {
+        for (int i = 1, j = 10; i < j; i++, j--) {
+            System.out.printf("%03d -- 503d\n", i, j);
+        }
+    }
+}
